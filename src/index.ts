4a2d254ba74735ee193aa7c84da59bcf6b1b0e4d
claire lin <clairelin@kklab.com>
2022-05-25 00:26:26 +0800

    Fixed toolbar item selected bug and refactored code

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,47 @@ import { Toolkit } from "./constant";
 import Canvas from "./canvas";
 
 let canvas = new Canvas(document.getElementById('canvas') as HTMLCanvasElement);
-const rectBtn = document.getElementById('rectangle')
-rectBtn?.addEventListener('click', () => {
-  rectBtn.classList.add('active');
-  if (canvas.state.currentToolkit !== Toolkit.RECTANGLE) {
-    canvas.unregisterEventListeners(canvas.state.currentToolkit, canvas);
+
+const nodes = [
+  {
+    node: document.getElementById('rectangle'),
+    listeners: {
+      click: () => {
+        if (canvas.state.currentToolkit !== Toolkit.RECTANGLE) {
+          canvas.unregisterEventListeners(canvas.state.currentToolkit, canvas);
+        }
+        canvas.registerEventListeners(Toolkit.RECTANGLE, canvas);
+        canvas.state.currentToolkit = Toolkit.RECTANGLE;
+      }
+    }
+  },
+  {
+    node: document.querySelector('.toolbar'),
+    listeners: {
+      click: (e: Event) => {
+        const toolbarItems = document.querySelectorAll('.toolbar-item');
+        toolbarItems.forEach(i => i.classList.remove('active'));
+        const target = e.target as Element;
+        target.classList.add('active');
+      }
+    }
+  },
+  {
+    node: document.getElementById('ellipse'),
+    listeners: {
+      click: () => {
+        if (canvas.state.currentToolkit !== Toolkit.ELLIPSE) {
+          canvas.unregisterEventListeners(canvas.state.currentToolkit, canvas);
+        }
+        canvas.registerEventListeners(Toolkit.ELLIPSE, canvas);
+        canvas.state.currentToolkit = Toolkit.ELLIPSE;
+      },
+    }
   }
-  canvas.registerEventListeners(Toolkit.RECTANGLE, canvas);
-  canvas.state.currentToolkit = Toolkit.RECTANGLE;
-})
-const ellipseBtn = document.getElementById('ellipse')
-ellipseBtn?.addEventListener('click', () => {
-  ellipseBtn.classList.add('active');
-  if (canvas.state.currentToolkit !== Toolkit.ELLIPSE) {
-    canvas.unregisterEventListeners(canvas.state.currentToolkit, canvas);
-  }
-  canvas.registerEventListeners(Toolkit.ELLIPSE, canvas);
-  canvas.state.currentToolkit = Toolkit.ELLIPSE;
+]
+
+nodes.forEach((nodeObj) => {
+  Object.entries(nodeObj.listeners).forEach(([key, value]) => {
+    nodeObj.node?.addEventListener(key, value)
+  })
 })
